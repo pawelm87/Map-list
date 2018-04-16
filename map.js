ff80912ff9bb118d2e087528e9b1c238797d8029
pawelm87 <pmmalypm@tlen.pl>
2018-04-16 11:13:09 +0200

    done
    done

---
+++ map.js
@@ -0,0 +1,100 @@
+const peopleFromServer = [
+    {
+        id: 1,
+        firstName: 'paul',
+        lastName: 'kow',
+        age: 31
+    },
+    {
+        id: 2,
+        firstName: 'luke',
+        lastName: 'kow',
+        age: 33
+    },
+    {
+        id: 3,
+        firstName: 'kris',
+        lastName: 'now',
+        age: 41
+    },
+    {
+        id: 4,
+        firstName: 'anna',
+        lastName: 'tom',
+        age: 31
+    },
+    {
+        id: 5,
+        firstName: 'luka',
+        lastName: 'kow',
+        age: 33
+    },
+];
+
+
+function getPersonById(map, id) {
+    for(let people of map.values()) {
+        for(let person of people){
+            if(person.id === id) {
+                return person;
+            }
+        }
+    } 
+}
+
+function addPersonToMap(map, person) {
+    const copyOfMapToAdd = copyMap(map);
+    if(copyOfMapToAdd.has(person.lastName)){
+        const people = copyOfMapToAdd.get(person.lastName);
+        if(!checkIfDuplicate(people, person)){
+            people.push(person);
+        }
+    } else {
+        copyOfMapToAdd.set(person.lastName, [person]);
+    }
+    return copyOfMapToAdd;
+}
+
+function copyMap(map) {
+    const copyOfMap = new Map();
+    for (let lastName of map.keys()) {
+        const people = [];
+        copyOfMap.set(lastName, people);
+        for(let person of map.get(lastName)){
+            people.push(person);
+        }
+    }   
+    return copyOfMap; 
+}
+
+function makeMap(peopleFromServer) {
+    let mapOfPeople = new Map();
+
+    for(let i = 0; i < peopleFromServer.length; i++) {
+        const personFormSerwer = peopleFromServer[i];
+        const lastNameOfPerson = personFormSerwer.lastName;
+
+        if(mapOfPeople.has(lastNameOfPerson)) {
+            if(!checkIfDuplicate(mapOfPeople.get(lastNameOfPerson), personFormSerwer)) {
+                mapOfPeople.get(lastNameOfPerson).push(personFormSerwer);
+            }
+        } else {
+                mapOfPeople.set(lastNameOfPerson, [personFormSerwer]);
+            }
+        }
+    return mapOfPeople;
+}
+
+function checkIfDuplicate(people, person) {
+    for(let j = 0;  j <people.length; j++) {
+        if(compareToPerson(people[j], person)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function compareToPerson(existPerson, newPerson) {
+    return (existPerson.firstName === newPerson.firstName && existPerson.lastName == newPerson.lastName 
+        && existPerson.age === newPerson.age);
+}
